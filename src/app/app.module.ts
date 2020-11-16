faffd4e21ff8f68cc330f359e54097afa50bb923
wil-bert <ianwilbuts@gmail.com>
2020-11-16 14:11:29 +0300

    initial pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentComponent
+    DepartmentComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
