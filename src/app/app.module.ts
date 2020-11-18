95189c39981eb885fd816ce55aaf904783cffb51
Bryson69 <brysonmundia@gmail.com>
2020-11-18 11:17:59 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeinfoComponent } from './employeeinfo/employeeinfo.component';
+import { UpduserdetailsComponent } from './upduserdetails/upduserdetails.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ManagerolesComponent } from './manageroles/manageroles.component';
+import { SearchdeptComponent } from './searchdept/searchdept.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeeinfoComponent,
+    UpduserdetailsComponent,
+    EmployeesComponent,
+    ManagerolesComponent,
+    SearchdeptComponent,
+    AdminhomeComponent
   ],
   imports: [
     BrowserModule,
