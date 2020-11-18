eca9cbdc19dec34d5591ae939a72deaec9d33be9
Bryson69 <brysonmundia@gmail.com>
2020-11-18 11:17:43 +0300

    Add routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { EmployeeinfoComponent } from './employeeinfo/employeeinfo.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ManagerolesComponent } from './manageroles/manageroles.component';
+import { SearchdeptComponent } from './searchdept/searchdept.component';
+import { UpduserdetailsComponent } from './upduserdetails/upduserdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AdminhomeComponent
+  },
+  {
+    path: 'search', component: SearchdeptComponent
+  },
+  {
+    path: 'role', component: ManagerolesComponent
+  },
+  {
+    path: 'user-detail', component: UpduserdetailsComponent
+  },
+  {
+    path: 'employees', component: EmployeesComponent
+  },
+  {
+    path: 'employee-info', component: EmployeeinfoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
