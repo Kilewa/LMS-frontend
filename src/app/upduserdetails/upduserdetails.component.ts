96667d0de0ac1bc0b1800213fa3c3847db40f099
Bryson69 <brysonmundia@gmail.com>
2020-11-18 11:15:52 +0300

    Add compontn upduserdetails

---
+++ src/app/upduserdetails/upduserdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upduserdetails',
+  templateUrl: './upduserdetails.component.html',
+  styleUrls: ['./upduserdetails.component.css']
+})
+export class UpduserdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
